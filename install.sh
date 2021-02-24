24eb6d8f4249cfc08db14a3b874d90526ec36219
root <root@localhost.localdomain>
2021-02-24 07:53:13 -0500

    Fuck

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt install python3 python3-pip -y
+apt install python python-pip -y
+git clone https://github.com/Kamakepar2029/ltcbot_kamakepar
+cd ltcbot_kamakepar
+bash start.sh
+cd ..
+echo "To start telebot, type: python3 bots.py +*************"
